6b13627437d0b202749ca7cd0b5eb7339892e717
psychobas <dominikmeier@outlook.com>
2020-08-05 18:44:55 +0200

    change particles color

--- starter_script.R
+++ starter_script.R
@@ -1,7 +1,2 @@
-install.packages("blogdown")
 library(blogdown)
-install_hugo()
-
-install_theme("nodejh/hugo-theme-cactus-plus", theme_example = TRUE, update_config = TRUE)
-
 blogdown::serve_site()
